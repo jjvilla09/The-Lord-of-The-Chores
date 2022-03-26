52d279f81fdb8b2c81776849b8636b72cd5752b4
Isaac <Isaac@DESKTOP-6OC0B2Q.utsarr.net>
2022-03-26 16:24:53 -0500

    Main Screen test

---
+++ TheLordOfTheChores/src/application/controller/MainController.java
@@ -0,0 +1,23 @@
+package application.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MainController {
+
+    @FXML
+    void questBoardHandle(ActionEvent event) {
+    	
+    }
+
+    @FXML
+    void inventoryHandle(ActionEvent event) {
+
+    }
+
+    @FXML
+    void shopHandle(ActionEvent event) {
+
+    }
+
+}
