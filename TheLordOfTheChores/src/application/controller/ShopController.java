a1ebb7eb5e3e60ed54f7a410bafe212d6b3b9ed6
jjvilla09 <99827936+jjvilla09@users.noreply.github.com>
2022-03-31 16:34:26 -0500

    This is a test

--- TheLordOfTheChores/src/application/controller/ShopController.java
+++ TheLordOfTheChores/src/application/controller/ShopController.java
@@ -3,16 +3,19 @@ package application.controller;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-public class ShopController {
+public class ShopController implements Initializable {
 	
 	@FXML
     private AnchorPane shopPane;	//pane
@@ -32,6 +35,12 @@ public class ShopController {
     	//display scene
     	window.show();
     }
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		GridPane gp = new GridPane();
+	}
 
 }
 
