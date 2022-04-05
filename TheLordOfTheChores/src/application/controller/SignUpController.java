91136f75a79170524fc46db5b589cfd91faf1ad8
DivinieBanhBao <isaacnguyen123@gmail.com>
2022-04-05 00:41:36 -0500

    Sign up and store login complete

--- TheLordOfTheChores/src/application/controller/SignUpController.java
+++ TheLordOfTheChores/src/application/controller/SignUpController.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import application.model.SignUp;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -19,57 +20,79 @@ import javafx.stage.Stage;
 
 public class SignUpController {
 
-	   @FXML
-	   private Label incorrectLogin;
+	@FXML
+	private TextField username;
+	
+	@FXML
+	private PasswordField password;	
+	
+	@FXML
+	private PasswordField confirmPassword;	
+	
+    @FXML
+    private Label invalid;
 
-	    @FXML
-	    private PasswordField confirmPassword;
+	@FXML
+	private AnchorPane mainPane;
 
-	    @FXML
-	    private PasswordField password;
+	
 
-	    @FXML
-	    private AnchorPane mainPane;
+	@FXML
+	void haveLoginHandle(ActionEvent event) throws IOException {
+		//get url file
+		URL url = new File("src/Login.fxml").toURI().toURL();
+		//load pane
+		mainPane = FXMLLoader.load(url);
+		//create new scene
+		Scene scene = new Scene(mainPane, 600, 400);
+		//set stage
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		//set scene
+		window.setScene(scene);
+		window.setScene(scene);
+		// QuestBoardBranch: set title
+		window.setTitle("The Lord of the Chores - Login");
+		//display scene
+		window.show();
+	}
 
-	    @FXML
-	    private TextField username;
-
-	    @FXML
-	    void haveLoginHandle(ActionEvent event) throws IOException {
-	    	//get url file
-	    	URL url = new File("src/Login.fxml").toURI().toURL();
-	    	//load pane
-	    	mainPane = FXMLLoader.load(url);
-	    	//create new scene
-	    	Scene scene = new Scene(mainPane, 600, 400);
-	    	//set stage
-	    	Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-	    	//set scene
-	    	window.setScene(scene);
-	    	window.setScene(scene);
-	    	// QuestBoardBranch: set title
-	    	window.setTitle("The Lord of the Chores - Login");
-	    	//display scene
-	    	window.show();
+	@FXML
+	void SignUpHandle(ActionEvent event) throws IOException {
+		
+		String user=username.getText();
+	    String pass=password.getText();
+	    String confirmpass=confirmPassword.getText();
+	    invalid.setText("");
+	    if(user.equals(null) || user.isEmpty() ||pass.equals(null) || pass.isEmpty()) {
+	    	invalid.setText("complete sign up");
+	    }
+	    else {
+	    	if(pass.equals(confirmpass))
+	    	{
+	    		SignUp su = new SignUp(user,pass);
+	    		su.storePassword();
+	    		//get url file
+	    		URL url = new File("src/Login.fxml").toURI().toURL();
+	    		//load pane
+	    		mainPane = FXMLLoader.load(url);
+	    		//create new scene
+	    		Scene scene = new Scene(mainPane, 600, 400);
+	    		//set stage
+	    		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+	    		//set scene
+	    		window.setScene(scene);
+	    		window.setScene(scene);
+	    		// QuestBoardBranch: set title
+	    		window.setTitle("The Lord of the Chores - Login");
+	    		//display scene
+	    		window.show();
+	    	}
+	    	else
+	    	{
+	    		invalid.setText("password does not match");	
+	    	}
 	    }
 
-	    @FXML
-	    void SignUpHandle(ActionEvent event) throws IOException {
-	    	//get url file
-	    	URL url = new File("src/Login.fxml").toURI().toURL();
-	    	//load pane
-	    	mainPane = FXMLLoader.load(url);
-	    	//create new scene
-	    	Scene scene = new Scene(mainPane, 600, 400);
-	    	//set stage
-	    	Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-	    	//set scene
-	    	window.setScene(scene);
-	    	window.setScene(scene);
-	    	// QuestBoardBranch: set title
-	    	window.setTitle("The Lord of the Chores - Login");
-	    	//display scene
-	    	window.show();
-	    }
+	}
 
 }
---
+++ TheLordOfTheChores/src/application/model/SignUp.java
@@ -0,0 +1,39 @@
+package application.model;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class SignUp {
+	private String username=null;
+	private String password=null;
+	//private static HashMap<String, String> store =new HashMap<String,String>();
+	
+	public SignUp(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void storePassword() throws IOException {
+		//store.put(this.username,this.password);
+		String acc = this.username + "," + this.password + "\n";
+		File file = new File("data/users.csv");
+		FileWriter wFile = new FileWriter(file,true);
+		System.out.println(acc);
+		wFile.write(acc);
+		wFile.close();
+	}
+}
