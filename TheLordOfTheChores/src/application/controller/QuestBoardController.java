53b8e3aecf4706dbaa2f3342c8c988fc584a1337
Nikhil Kapoor <nikhil.kapoor@my.utsa.edu>
2022-04-13 12:38:58 -0500

    added Help function and imported a new image into the data folder

--- TheLordOfTheChores/src/application/controller/QuestBoardController.java
+++ TheLordOfTheChores/src/application/controller/QuestBoardController.java
@@ -1,3 +1,9 @@
+/**
+ * Creator: Nikhil Kapoor (yct482)
+ * File: QuestBoardController.java
+ * Purpose: Controls the Quest Board scene, using QuestBoard class objects to allow
+ * users to create quests, complete them, and receive rewards.
+ */
 package application.controller;
 
 import java.io.File;
@@ -9,6 +15,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -21,10 +29,13 @@ public class QuestBoardController {
     private AnchorPane questBoardPane;	// -questBoardPane: AnchorPane
 	
 	@FXML
-    private ImageView toHomeImage; // -toHomeImage: ImageView
+    private ImageView toHomeImage, toHelpImage; // -toHomeImage, -toHelpImage: ImageView
 	
 	@FXML
     private Button createNewQuestButton; // -createNewQuestButton: Button
+	
+	@FXML
+	private Alert a = new Alert(AlertType.NONE);
 	
     @FXML
     void toHomeHandle(MouseEvent event) throws IOException {
@@ -51,6 +62,26 @@ public class QuestBoardController {
     void createNewQuest(ActionEvent event) {
     	if (event.getSource() == createNewQuestButton) {
     		System.out.println("Hey look at me!");
+    	}
+    }
+    
+    @FXML
+    void handleHelp(MouseEvent event) {
+    	if (event.getSource() == toHelpImage) {
+    		System.out.println("toHelpImage pressed");
+    		a.setAlertType(AlertType.INFORMATION);
+    		a.setContentText("The main goal of The Lord of the Chores is to encourage gamers to take up\n"
+    				+ "any hobbies or daily goals to improve both physical and mental health. Here, at TLOTC Inc.,\n"
+    				+ "we want the user to create their own quests to better encourage completion.\n"
+    				+ "HOW TO:\n\n"
+    				+ "Step 1: Enter the Title of the Quest. It can be anything.\n"
+    				+ "Step 2: Select a difficulty, from Easy, Medium, and Hard. The difficulty determines"
+    				+  " the amount of Gold obtained from completing the quest.\n"
+    				+ "Step 3: Press the Create New Quest button after filling in these two fields and the quest"
+    				+ " should create a quest down at the bottom.\n"
+    				+ "\nYou can only create one quest at a time, so don't try and overwork youself by creating multiple quests."
+    				+ "\n\nHappy Trails!");
+    		a.showAndWait();
     	}
     }
 
